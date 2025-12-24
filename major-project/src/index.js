cc0779f9f1e3a684388e7509c4f8f63efae9747f
Aryan Jha <aryans2637@gmail.com>
2025-12-24 09:28:22 +0530

    Api error and Response

--- major-project/src/index.js
+++ major-project/src/index.js
@@ -7,6 +7,15 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>[
+
+        console.log(`Server running on port ${process.env.PORT}`)
+    ])
+})
+.catch((err)=>{
+    console.log("MONGODB connection failed!!!",err);
+})
 
 
 
