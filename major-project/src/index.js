8d91b662d1801611fd7cab558632611f2b2ca0d2
Aryan Jha <aryans2637@gmail.com>
2025-12-25 11:48:00 +0530

    user controller and router

--- major-project/src/index.js
+++ major-project/src/index.js
@@ -1,23 +1,25 @@
 //require('dotenv').config({path:'./env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import {app} from "./app.js"
 
 dotenv.config({
     path:'./env'
 })
 
-connectDB()
-.then(()=>{
-    app.listen(process.env.PORT||8000,()=>[
-
-        console.log(`Server running on port ${process.env.PORT}`)
-    ])
-})
-.catch((err)=>{
-    console.log("MONGODB connection failed!!!",err);
-})
+app.get("/ping", (req, res) => {
+  res.status(200).send("pong");
+});
 
 
+
+app.listen(process.env.PORT||8000,()=>{
+    console.log("Server started without DB");
+    
+}
+)
+
+connectDB().catch(console.error)
 
 
 
